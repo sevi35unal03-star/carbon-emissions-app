5af07bc77804226c18ef69535022bf1da4ffa384
Dilara Ceyda Çetin <dilaraceydacetinn@gmail.com>
2026-03-13 13:14:38 +0300

    fix(domain): add IDomainEventContainer for polymorphic event dispatch
    - Add IDomainEventContainer interface to Domain/Common
    - BaseEntity implements IDomainEventContainer
    - User implements IDomainEventContainer (IdentityUser turevı, BaseEntity'den tureyemiyor)
    - DispatchDomainEventsInterceptor scans IDomainEventContainer instead of BaseEntity
    - Fixes: UserRegisteredDomainEvent, UserDeletedDomainEvent never dispatched

--- src/IzTek.Carbon.Footprint.Domain/Common/BaseEntity.cs
+++ src/IzTek.Carbon.Footprint.Domain/Common/BaseEntity.cs
@@ -1,8 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
-
 namespace IzTek.Carbon.Footprint.Domain.Common;
 
-public abstract class BaseEntity
+public abstract class BaseEntity : IDomainEventContainer  // ← eklendi
 {
     public Guid Id { get; set; } = Guid.CreateVersion7();
 
@@ -10,7 +9,6 @@ public abstract class BaseEntity
 
     [NotMapped]
     public IReadOnlyCollection<BaseEvent> DomainEvents => _domainEvents.AsReadOnly();
-
     public void AddDomainEvent(BaseEvent domainEvent) => _domainEvents.Add(domainEvent);
     public void RemoveDomainEvent(BaseEvent domainEvent) => _domainEvents.Remove(domainEvent);
     public void ClearDomainEvents() => _domainEvents.Clear();
