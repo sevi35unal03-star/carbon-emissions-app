d11d160b72a9adc708c7bc8d7041b2a0aace5b22
Dilara Ceyda Çetin <dilaraceydacetinn@gmail.com>
2026-03-26 09:31:36 +0300

    refactor: remove GetGoalDetail and GetPreviousGoals features
    - Remove GetGoalDetail query, handler and response
    - Remove GetPreviousGoals query, handler and response
    - These features are not in Figma design

--- src/IzTek.Carbon.Footprint.Application/Features/Results/Queries/GetGoalDetail/GetGoalDetailQuery.cs
+++
@@ -1,5 +0,0 @@
-﻿using IzTek.Carbon.Footprint.Application.Common.Constants;
-
-namespace IzTek.Carbon.Footprint.Application.Features.Results.Queries.GetGoalDetail;
-
-public record GetGoalDetailQuery(int Month, int Year);
--- src/IzTek.Carbon.Footprint.Application/Features/Results/Queries/GetPreviousGoals/GetPreviousGoalsQuery.cs
+++
@@ -1,3 +0,0 @@
-﻿namespace IzTek.Carbon.Footprint.Application.Features.Results.Queries.GetPreviousGoals;
-
-public record GetPreviousGoalsQuery(Guid UserId);
