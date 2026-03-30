294ab9353139d6bb37463092310a75c3453e6787
Dilara Ceyda Çetin <dilaraceydacetinn@gmail.com>
2026-03-30 12:57:19 +0300

    fix: PollOptions direkt contexte eklendi. - ServiceCollectionExtensions.cs - PollSet + PollQuestion + PollOption ve confifuration dosyalari guncellendi.

---
+++ src/IzTek.Carbon.Footprint.Persistence/Migrations/20260330093149_SyncModel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IzTek.Carbon.Footprint.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PollOptions_PollQuestions_NextPollQuestionId",
+                table: "PollOptions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PollOptions_NextPollQuestionId",
+                table: "PollOptions");
+        }
+    }
+}
