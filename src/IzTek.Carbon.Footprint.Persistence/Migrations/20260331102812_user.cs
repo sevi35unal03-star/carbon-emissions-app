5b27cd5861e87b8546398ca16cec8c3cf7954605
Sevgi Ünal <sevi35unal03@gmail.com>
2026-03-31 14:57:43 +0300

    features: draft ve draft2 migramation history silindi user migramationda up metodu düzeltildi

---
+++ src/IzTek.Carbon.Footprint.Persistence/Migrations/20260331102812_user.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IzTek.Carbon.Footprint.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class user : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Kolon zaten varsa atla
+            migrationBuilder.Sql(@"
+        ALTER TABLE ""UserPollResults"" 
+        ADD COLUMN IF NOT EXISTS ""IsCompleted"" boolean NOT NULL DEFAULT false;
+    ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsCompleted",
+                table: "UserPollResults");
+        }
+    }
+}
