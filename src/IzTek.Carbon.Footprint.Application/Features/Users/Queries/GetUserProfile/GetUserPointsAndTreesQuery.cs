805f123cd2dda73c0d5704e578c7517fbe32abc8
Sevgi Ünal <sevi35unal03@gmail.com>
2026-04-09 11:38:18 +0300

    Hatalı kısımları düzelttim

--- src/IzTek.Carbon.Footprint.Application/Features/Users/Queries/GetUserProfile/GetUserPointsAndTreesQuery.cs
+++ src/IzTek.Carbon.Footprint.Application/Features/Users/Queries/GetUserProfile/GetUserPointsAndTreesQuery.cs
@@ -1,5 +1,4 @@
-﻿using IzTek.Carbon.Footprint.Application.Common.Models;
-namespace IzTek.Carbon.Footprint.Application.Features.Users.Queries.GetUserProfile
+﻿namespace IzTek.Carbon.Footprint.Application.Features.Users.Queries.GetUserProfile
 {
     public record GetUserPointsAndTreesQuery;
 }
