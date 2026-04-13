3fae18f98d5f03c78dcc40dca0d303aaf1856e45
Dilara Ceyda Çetin <dilaraceydacetinn@gmail.com>
2026-04-13 10:41:04 +0300

    fix: DomainException handling improvements
    - Extend DomainException from ApplicationException
    - Add DomainException to GlobalExceptionHandler

--- src/IzTek.Carbon.Footprint.Domain/Common/Exceptions/DomainException.cs
+++ src/IzTek.Carbon.Footprint.Domain/Common/Exceptions/DomainException.cs
@@ -1,8 +1,10 @@
 ﻿namespace IzTek.Carbon.Footprint.Domain.Common.Exceptions;
 
-public class DomainException : Exception
+public class DomainException : ApplicationException
 {
-    public DomainException(string message) : base(message) { }
+    public DomainException(string message) : base(message)
+    {
+    }
 
     public DomainException(string message, Exception innerException)
         : base(message, innerException) { }
