aca7961c1dee7e4e0f82a361aece84818a909bc1
Dilara Ceyda Çetin <dilaraceydacetinn@gmail.com>
2026-04-16 09:32:42 +0300

    refactor: Mapster kaldirildi, manuel yapildi.

--- src/IzTek.Carbon.Footprint.Application/Features/ActivityQuestions/Queries/ActivityQuestionResponse.cs
+++ src/IzTek.Carbon.Footprint.Application/Features/ActivityQuestions/Queries/ActivityQuestionResponse.cs
@@ -7,6 +7,6 @@ public record ActivityQuestionResponse
     public int DisplayOrder { get; set; }
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
-    public TimeSpan NotificationTime { get; set; }
+    public TimeSpan ScheduledTime { get; set; }
     public List<ActivityOptionResponse> Options { get; set; } = new();
 }
